081b2e5805dd57b15cd711e5d0ddab0d2d01c098
Asif Ali <asif.ali14280@gmail.com>
2022-11-03 09:25:07 +0500

    chnages

--- src/pages/Projectsdata.js
+++ src/pages/Projectsdata.js
@@ -1,37 +1,37 @@
 const Projectsdata = [
     {
         title: 'Global Fansy',
-        image: './download.jpeg',
+        image: './socialmedia.svg',
         description: '',
         link: 'https://www.globalfansy.com/'
     },
     {
+        title: 'Taaply',
+        image: './socialmedia.svg',
+        description: '',
+        link: 'https://taaply.com/'
+    },
+    {
         title: 'SHEYSHOP',
-        image: './download.jpeg',
+        image: './socialmedia.svg',
         description: '',
         link: ''
     },
     {
         title: 'SHEYSHOP',
-        image: './download.jpeg',
+        image: './socialmedia.svg',
         description: '',
         link: ''
     },
     {
         title: 'SHEYSHOP',
-        image: './download.jpeg',
+        image: './socialmedia.svg',
         description: '',
         link: ''
     },
     {
         title: 'SHEYSHOP',
-        image: './download.jpeg',
-        description: '',
-        link: ''
-    },
-    {
-        title: 'SHEYSHOP',
-        image: './download.jpeg',
+        image: './socialmedia.svg',
         description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
         link: ''
     },
