4438ac8086f80dbc4f2aaf6bdbd49189fb5f6599
Asif Ali <asif.ali14280@gmail.com>
2022-11-03 09:34:23 +0500

    chnages

--- src/pages/Projectsdata.js
+++ src/pages/Projectsdata.js
@@ -2,111 +2,111 @@ const Projectsdata = [
     {
         title: 'Global Fansy',
         image: './socialmedia.svg',
-        description: '',
+        description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
         link: 'https://www.globalfansy.com/'
     },
     {
         title: 'Taaply',
         image: './socialmedia.svg',
-        description: '',
+        description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
         link: 'https://taaply.com/'
     },
     {
         title: 'MTN Taaply',
         image: './socialmedia.svg',
-        description: '',
+        description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
         link: 'https://mtn-taaply.netlify.app/'
     },
     {
         title: 'Fycle',
         image: './socialmedia.svg',
-        description: '',
+        description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
         link: 'https://fycle.netlify.app/'
     },
     {
         title: 'Render Health',
         image: './socialmedia.svg',
-        description: 'https://www.renderhealth.com/',
+        description: 'Reference site about Lorem Ipsum, giving information on its origins, as wellhttps://www.renderhealth.com/',
         link: ''
     },
     {
         title: 'AMS',
         image: './socialmedia.svg',
-        description: '',
+        description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
         link: 'https://www.armormatrixsystems.com/'
     },
     {
         title: 'Admin Dahsboard',
         image: './socialmedia.svg',
-        description: '',
+        description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
         link: 'https://admin-dashboard-responsive.netlify.app/'
     },
     {
         title: 'The6ixClub',
         image: './socialmedia.svg',
-        description: '',
+        description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
         link: 'https://the6ixclub.com/'
     },
     {
         title: 'The Medicators',
         image: './socialmedia.svg',
-        description: '',
+        description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
         link: 'https://themedicators.netlify.app/'
     },
     {
         title: 'ARHT',
         image: './socialmedia.svg',
-        description: '',
+        description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
         link: 'https://www.arht.tw/'
     },
     {
         title: 'Proelean',
         image: './socialmedia.svg',
-        description: '',
+        description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
         link: 'https://dex.proelean.com/'
     },
     {
         title: '1TimeUsed',
         image: './socialmedia.svg',
-        description: '',
+        description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
         link: 'https://1timeused.com/'
     },
     {
         title: 'Lusso And Vinci',
         image: './socialmedia.svg',
-        description: '',
+        description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
         link: 'https://lussoandvinci.com/'
     },
     {
         title: 'Visiolab',
         image: './socialmedia.svg',
-        description: '',
+        description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
         link: 'https://visiolab.io/'
     },
     {
         title: 'Brazosgrafix',
         image: './socialmedia.svg',
-        description: '',
+        description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
         link: 'http://www.brazosgrafix.com/'
     },
     {
         title: 'Infusedh2o',
         image: './socialmedia.svg',
-        description: '',
+        description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
         link: 'https://www.infusedh2o.se/'
     },
     {
         title: 'Premiermedicaid',
         image: './socialmedia.svg',
-        description: '',
+        description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
         link: 'https://premiermedicaid.com/'
     },
     {
         title: 'Bamkoles',
         image: './socialmedia.svg',
-        description: '',
+        description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
         link: 'https://bamkoles.com/'
-    },
+    }
 ];
 
 export default Projectsdata;
