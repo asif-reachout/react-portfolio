a19ebdc1707856e27e59d1559094c72d271b05f3
Asif Ali <asif.ali14280@gmail.com>
2022-11-03 09:23:06 +0500

    chnages

--- src/pages/Projectsdata.js
+++ src/pages/Projectsdata.js
@@ -1,39 +1,39 @@
-const Projectsdata =[
+const Projectsdata = [
     {
-        title:'SHEYSHOP',
-        image:'./ecommerce.svg',
-        description:'An ecommerce application developed using react , node , redux and mongodb',
-        link:''
+        title: 'Global Fansy',
+        image: './download.jpeg',
+        description: '',
+        link: 'https://www.globalfansy.com/'
     },
     {
-        title:'SHEYSHOP',
-        image:'./pizzas.svg',
-        description:'An ecommerce application developed using react , node , redux and mongodb',
-        link:''
+        title: 'SHEYSHOP',
+        image: './download.jpeg',
+        description: '',
+        link: ''
     },
     {
-        title:'SHEYSHOP',
-        image:'./socialmedia.svg',
-        description:'An ecommerce application developed using react , node , redux and mongodb',
-        link:''
+        title: 'SHEYSHOP',
+        image: './download.jpeg',
+        description: '',
+        link: ''
     },
     {
-        title:'SHEYSHOP',
-        image:'./hotels.svg',
-        description:'An ecommerce application developed using react , node , redux and mongodb',
-        link:''
+        title: 'SHEYSHOP',
+        image: './download.jpeg',
+        description: '',
+        link: ''
     },
     {
-        title:'SHEYSHOP',
-        image:'./cars.svg',
-        description:'An ecommerce application developed using react , node , redux and mongodb',
-        link:''
+        title: 'SHEYSHOP',
+        image: './download.jpeg',
+        description: '',
+        link: ''
     },
     {
-        title:'SHEYSHOP',
-        image:'./jobs.svg',
-        description:'Reference site about Lorem Ipsum, giving information on its origins, as well',
-        link:''
+        title: 'SHEYSHOP',
+        image: './download.jpeg',
+        description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
+        link: ''
     },
 ];
 
