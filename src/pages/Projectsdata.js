e069d920a04e81ca627fbef6907d40a923c7d741
Asif Ali <asif.ali14280@gmail.com>
2022-11-03 09:33:08 +0500

    chnages

--- src/pages/Projectsdata.js
+++ src/pages/Projectsdata.js
@@ -12,28 +12,100 @@ const Projectsdata = [
         link: 'https://taaply.com/'
     },
     {
-        title: 'SHEYSHOP',
+        title: 'MTN Taaply',
         image: './socialmedia.svg',
         description: '',
+        link: 'https://mtn-taaply.netlify.app/'
+    },
+    {
+        title: 'Fycle',
+        image: './socialmedia.svg',
+        description: '',
+        link: 'https://fycle.netlify.app/'
+    },
+    {
+        title: 'Render Health',
+        image: './socialmedia.svg',
+        description: 'https://www.renderhealth.com/',
         link: ''
     },
     {
-        title: 'SHEYSHOP',
+        title: 'AMS',
         image: './socialmedia.svg',
         description: '',
-        link: ''
+        link: 'https://www.armormatrixsystems.com/'
     },
     {
-        title: 'SHEYSHOP',
+        title: 'Admin Dahsboard',
         image: './socialmedia.svg',
         description: '',
-        link: ''
+        link: 'https://admin-dashboard-responsive.netlify.app/'
     },
     {
-        title: 'SHEYSHOP',
+        title: 'The6ixClub',
         image: './socialmedia.svg',
-        description: 'Reference site about Lorem Ipsum, giving information on its origins, as well',
-        link: ''
+        description: '',
+        link: 'https://the6ixclub.com/'
+    },
+    {
+        title: 'The Medicators',
+        image: './socialmedia.svg',
+        description: '',
+        link: 'https://themedicators.netlify.app/'
+    },
+    {
+        title: 'ARHT',
+        image: './socialmedia.svg',
+        description: '',
+        link: 'https://www.arht.tw/'
+    },
+    {
+        title: 'Proelean',
+        image: './socialmedia.svg',
+        description: '',
+        link: 'https://dex.proelean.com/'
+    },
+    {
+        title: '1TimeUsed',
+        image: './socialmedia.svg',
+        description: '',
+        link: 'https://1timeused.com/'
+    },
+    {
+        title: 'Lusso And Vinci',
+        image: './socialmedia.svg',
+        description: '',
+        link: 'https://lussoandvinci.com/'
+    },
+    {
+        title: 'Visiolab',
+        image: './socialmedia.svg',
+        description: '',
+        link: 'https://visiolab.io/'
+    },
+    {
+        title: 'Brazosgrafix',
+        image: './socialmedia.svg',
+        description: '',
+        link: 'http://www.brazosgrafix.com/'
+    },
+    {
+        title: 'Infusedh2o',
+        image: './socialmedia.svg',
+        description: '',
+        link: 'https://www.infusedh2o.se/'
+    },
+    {
+        title: 'Premiermedicaid',
+        image: './socialmedia.svg',
+        description: '',
+        link: 'https://premiermedicaid.com/'
+    },
+    {
+        title: 'Bamkoles',
+        image: './socialmedia.svg',
+        description: '',
+        link: 'https://bamkoles.com/'
     },
 ];
 
